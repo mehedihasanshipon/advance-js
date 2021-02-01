479ce875dedf3ee9e50cb37b3bb2ad8735b06273
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-02-02 01:44:13 +0600

    Null vs Undefined

---
+++ null-undefined.js
@@ -0,0 +1,9 @@
+// Undefined
+var demo;
+alert(demo); //shows undefined
+alert(typeof demo); //shows undefined
+
+// Null
+var demo = null;
+alert(demo); //shows null
+alert(typeof demo); //shows object
