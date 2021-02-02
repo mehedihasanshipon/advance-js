9eb312bdf16fc0705b01c6593eee1131a7aa9c16
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-02-02 18:35:34 +0600

    Break Continue

---
+++ break-continue.js
@@ -0,0 +1,15 @@
+const nums = [1,2,3,4,5,6,7,8];
+for(let i = 0; i<nums.length;i++){
+    if( nums[i]>5){
+        break;  
+    }
+    //  console.log(nums[i]);
+}
+
+const nums1 = [1,-2,3,-4,-5,6,7,8];
+for(let i =0;i<nums1.length;i++){
+    if(nums1[i]<0){
+        continue;
+    }
+    console.log(nums1[i]);
+}
