f6d2263d5bb1b46244fc26ee0158bfe8b6d25fb9
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-02-02 17:58:42 +0600

    Array split

---
+++ array-split.js
@@ -0,0 +1,10 @@
+// array slice
+const numbers = [1,2,3,4,5,6,7,8];
+// const part = numbers.slice(2,4);
+// array-splice
+const part = numbers.splice(2,4,10);
+// console.log(part);
+// console.log(numbers);
+// array join
+const joint = numbers.join('|');
+console.log(joint);
